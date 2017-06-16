98c19f8c5aee4280035a1b1eaa24d63afa4a7d47
Craig James Duffy <craigjduffy@Tungsten.local>
2017-06-16 20:40:44 +0100

    Layout-y things.

--- app/src/main/java/com/veloroute/duffylamb/veloroute/mainScreen.java
+++ app/src/main/java/com/veloroute/duffylamb/veloroute/mainScreen.java
@@ -1,73 +1,101 @@
 package com.veloroute.duffylamb.veloroute;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 
 import com.mapzen.android.graphics.MapFragment;
 import com.mapzen.android.graphics.MapzenMap;
 import com.mapzen.android.graphics.OnMapReadyCallback;
+import com.mapzen.android.graphics.model.CinnabarStyle;
 import com.mapzen.tangram.LngLat;
+
 
 
 public class mainScreen extends AppCompatActivity {
 
-    MapzenMap map;
-    /**
-     * To conserve resources, {@link MapzenMap#setMyLocationEnabled} is set to false when
-     * the activity is paused and re-enabled when the activity resumes.
-     */
+    private static final int PERMISSIONS_REQUEST_CODE = 1;
+    private MapzenMap map;
     private boolean enableLocationOnResume = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-
-        final MapFragment mapFragment =
-                (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        final MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapzenMap map) {
-                map.setPosition(new LngLat(-3.183814, 55.958723));
+                mainScreen.this.map = map;
                 map.setRotation(0f);
-                map.setZoom(17f);
+                map.setZoom(0);
                 map.setTilt(0f);
+                map.setPosition(new LngLat(0,0));
+                map.setZoomButtonsEnabled(true);
+                map.setCompassButtonEnabled(true);
+                checkRuntimePermissions();
+                map.setStyle(new CinnabarStyle());
 
             }
         });
     }
 
-    @Override protected void onPause() {
+    @Override
+    protected void onPause() {
         super.onPause();
-        if (map != null && map.isMyLocationEnabled()) {
+        if (map.isMyLocationEnabled()) {
             map.setMyLocationEnabled(false);
+            enableLocationOnResume = true;
         }
     }
 
-    private void configureMap(boolean enabled) {
-        if (enabled) {
-            setMyLocationEnabled(true);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (enableLocationOnResume) {
+            map.setMyLocationEnabled(true);
         }
-        map.setFindMeOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(mainScreen.this, R.string.change_location, Toast.LENGTH_SHORT)
-                        .show();
-            }
-        });
-
-        map.setCompassButtonEnabled(true);
-        map.setZoomButtonsEnabled(true);
-        map.setPersistMapState(true);
     }
 
-    private void setMyLocationEnabled(boolean enabled) {
-        map.setMyLocationEnabled(enabled);
-        enableLocationOnResume = enabled;
+    private boolean permissionNotGranted() { // Returns true if permission is not granted #TheArtOftheBodge
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) || (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED);
     }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
+        }, PERMISSIONS_REQUEST_CODE);
+    }
+
+    public void checkRuntimePermissions() {
+        if (permissionNotGranted()) {
+            requestPermission();
+        }
+        else {
+            map.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+
+        for(int i = 0; i<grantResults.length; i++){
+            if(grantResults[i] == PackageManager.PERMISSION_DENIED)
+                return;
+        }
+        map.setMyLocationEnabled(true);
+    }
+
+
 }
 
 
